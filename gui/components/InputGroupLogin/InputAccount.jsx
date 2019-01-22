006e9af84a8591ff930a4ede22d487f46dcb0063
chenlizan <clz_318@live.com>
2019-01-22 22:36:28 +0800

    账号下拉菜单效果

--- gui/components/InputGroupLogin/InputAccount.jsx
+++ gui/components/InputGroupLogin/InputAccount.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Avatar} from 'antd';
 import Menu, {MenuItem} from "rc-menu";
 import Trigger from 'rc-trigger';
 import IconFont from '../IconFont';
@@ -13,21 +14,53 @@ function getPopupContainer(trigger) {
     return trigger.parentNode;
 }
 
-function renderMenu() {
-    return (
-        <Menu>
-            <MenuItem>i do not need key</MenuItem>
-        </Menu>
-    );
-}
 
 export default class InputAccount extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedKeys: []
+        }
+    }
+
+
+    handleOnMouseEnter = ({key, domEvent}) => {
+        this.setState({
+            selectedKeys: [key]
+        })
+    };
+
+    handleSize = (key) => {
+        const {selectedKeys} = this.state;
+        if (key === selectedKeys[0])
+            return 'large';
+        else if (key == (Number(selectedKeys[0]) + 1) || key == (Number(selectedKeys[0]) - 1))
+            return 'default';
+        else
+            return 'small';
+    }
+
+    renderMenu = () => {
+        return (
+            <Menu className={styles['InputGroupLogin-Account-Menu']} selectedKeys={this.state.selectedKeys}>
+                <MenuItem key="1" onMouseEnter={this.handleOnMouseEnter}><Avatar size={this.handleSize("1")}
+                                                                                 src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/></MenuItem>
+                <MenuItem key="2" onMouseEnter={this.handleOnMouseEnter}><Avatar size={this.handleSize("2")}
+                                                                                 src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/></MenuItem>
+                <MenuItem key="3" onMouseEnter={this.handleOnMouseEnter}><Avatar size={this.handleSize("3")}
+                                                                                 src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/></MenuItem>
+            </Menu>
+        );
+    }
+
+
     render() {
         return (
             <div className={styles['InputGroupLogin-Account']}>
                 <Trigger
                     action={['click']}
-                    popup={renderMenu()}
+                    popup={this.renderMenu()}
                     popupAlign={{
                         points: ['tc', 'bc'],
                     }}
